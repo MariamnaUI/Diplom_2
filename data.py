83fcbe40ddc8917b2db686e29dae8caa06ba4f18
mariamna <mariamnaui@yandex.ru>
2024-11-25 22:27:21 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,5 @@
+BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
+CREATE_COURIER_URL = f'{BASE_URL}/courier'
+LOGIN_COURIER_URL = f'{BASE_URL}/courier/login'
+DELETE_COURIER_URL = f'{BASE_URL}/courier/:id'
+ORDER_URL = f'{BASE_URL}/orders'
